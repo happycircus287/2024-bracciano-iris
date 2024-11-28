3f9037150982d5aaeb8426956124b0805a3a4115
vian <vian@example.com>
2024-11-28 16:10:53 +0100

    add petal plot

--- plot_iris.py
+++ plot_iris.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 # Caricamento del dataset
 iris = load_iris()
+print(iris)
 X = iris.data
 y = iris.target
 
@@ -12,13 +13,21 @@ y = iris.target
 plt.figure(figsize=(12, 4))
 
 # Plot per i sepali
-plt.subplot(1, 2, 1)
+plt.subplot(2, 1, 2)
 for i in range(3):
     mask = y == i
     plt.scatter(X[mask, 0], X[mask, 1], label=iris.target_names[i])
 plt.xlabel('Lunghezza sepalo')
 plt.ylabel('Larghezza sepalo')
 plt.title('Distribuzione dei Sepali')
+plt.legend()
+plt.subplot(2, 1, 1)
+for i in range(3):
+    mask = y == i
+    plt.scatter(X[mask, 2], X[mask, 3], label=iris.target_names[i])
+plt.xlabel('Lunghezza Petalo')
+plt.ylabel('Larghezza Petalo')
+plt.title('Distribuzione dei Petali')
 plt.legend()
 
 # Plot per i petali
